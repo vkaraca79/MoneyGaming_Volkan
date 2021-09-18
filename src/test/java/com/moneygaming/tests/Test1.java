0852680b62d290ca4ef1e713c3116706e4d0f022
Volkan K <vkaraca79@hotmail.com>
2021-09-18 18:42:40 +0300

    I finished the first part of my project

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,30 +1,44 @@
 package com.moneygaming.tests;
 
 import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
+    WebDriver driver;
+
     @BeforeMethod
     public void setUp(){
 
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
-
+        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
 
     }
 
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
+
+        WebElement nameBox = driver.findElement(By.cssSelector("#forename"));
+        nameBox.sendKeys("Volkan");
+
+        WebElement surnameBox = driver.findElement(By.cssSelector("[name='map(lastName)']"));
+        surnameBox.sendKeys("Karaca");
+
+        Thread.sleep(3000);
 
     }
+
     @AfterMethod
     public void tearDown(){
 
+        driver.close();
     }
 
 }
