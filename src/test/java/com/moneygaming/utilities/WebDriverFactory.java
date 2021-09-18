cd138973c3b9b083053b0cd7c4ad8a40bfb14db8
Volkan K <vkaraca79@hotmail.com>
2021-09-18 18:06:11 +0300

    I added my WebDriverFactory utility class

---
+++ src/test/java/com/moneygaming/utilities/WebDriverFactory.java
@@ -0,0 +1,38 @@
+package com.moneygaming.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+
+public class WebDriverFactory {
+
+    // Task:
+    // Write a static method that takes a string parameter name: browserType
+    // based on the value of parameter
+    // it will setup the browser and
+    // the method will return chromedriver or foxdriver object
+    // name of the method: getDriver
+
+    public static WebDriver getDriver(String browserType){
+
+        WebDriver driver = null;
+
+        switch(browserType.toLowerCase()){
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+        }
+
+        return driver;
+    }
+}
